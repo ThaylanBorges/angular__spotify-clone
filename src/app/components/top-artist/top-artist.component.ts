83c668c5d975fb2fc1679e0405662bd40bb7febd
ThaylanBorges <taylan-150963@hotmail.com>
2024-05-27 16:07:43 -0300

    Creation of the carousel at Home.

--- src/app/components/top-artist/top-artist.component.ts
+++ src/app/components/top-artist/top-artist.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { faArrowAltCircleLeft, faArrowAltCircleRight } from '@fortawesome/free-solid-svg-icons';
 import { newArtist } from 'src/app/Common/factories';
 import { Iartist } from 'src/app/interfaces/Iartist';
+import { Iartists } from 'src/app/interfaces/Iartists';
 import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
@@ -10,11 +12,17 @@ import { SpotifyService } from 'src/app/services/spotify.service';
 })
 export class TopArtistComponent {
   topArtist: Iartist = newArtist();
+  topFiveArtists: Iartists[] = [];
+  selectedIndex = 0;
+  indicators = true;
+  faArrowLeft = faArrowAltCircleLeft
+  faArrowRight = faArrowAltCircleRight
 
   constructor(private spotifyService: SpotifyService) {}
 
   ngOnInit() {
     this.searchArtist();
+    this.searchFiveArtists();
   }
 
   async searchArtist() {
@@ -24,6 +32,31 @@ export class TopArtistComponent {
       this.topArtist = artists[0];
     } else {
       console.log('Artistas não encontrados');
+    }
+  }
+
+  async searchFiveArtists() {
+    this.topFiveArtists = await this.spotifyService.searchTopArtists(5);
+    console.log(this.topFiveArtists);
+  }
+
+  selectImage(index: number): void {
+    this.selectedIndex = index;
+  }
+
+  onPrevClick() {
+    if (this.selectedIndex === 0) {
+      this.selectedIndex = this.topFiveArtists.length - 1;
+    } else {
+      this.selectedIndex--;
+    }
+  }
+
+  onNextClick() {
+    if (this.selectedIndex === this.topFiveArtists.length -1) {
+      this.selectedIndex = 0;
+    } else {
+      this.selectedIndex++;
     }
   }
 }
---
+++ src/app/interfaces/Iartists.ts
@@ -0,0 +1,4 @@
+export interface Iartists {
+  name: string;
+  imageUrl: string;
+}
